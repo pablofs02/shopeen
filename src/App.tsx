1c8df491b1fe9806fdcc61e2763976301dba68b9
guillermo-tellRM <guillermotellg@gmail.com>
2023-03-02 13:48:49 +0100

    Relocated routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Container } from "react-bootstrap/";
+import About from "./components/About";
+import Help from "./components/Help";
+import Home from "./components/Home";
+import Store from "./components/Store";
 
 function App() {
-  return <BrowserRouter>
-    <Navbar></Navbar>
-    <h1>Contenido</h1>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/store" element={<Store />}></Route>
+        <Route path="/help" element={<Help />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,31 @@
-import { Row, Button, Container, Col } from "react-bootstrap/"
-import { Routes, Route, Link } from "react-router-dom"
-import About from "./About"
-import Help from "./Help"
-import Home from "./Home"
-import Store from "./Store"
+import { Row, Button, Container, Col } from "react-bootstrap/";
+import { Routes, Route, Link } from "react-router-dom";
+import About from "./About";
+import Help from "./Help";
+import Home from "./Home";
+import Store from "./Store";
 
 function Navbar() {
-    return <>
-        <Container className="d-flex justify-content-center align-items-center">
-            <Row>
-                <Col>
-                    <Link to={"/"}>Home</Link>
-                </Col>
-                <Col>
-                    <Link to={"/about"}>About</Link>
-                </Col>
-                <Col>
-                    <Link to={"/store"}>Store</Link>
-                </Col>
-                <Col>
-                    <Link to={"/help"}>Help</Link>
-                </Col>
-
-            </Row>
-        </Container>
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/about" element={<About/>}></Route>
-            <Route path="/store" element={<Store/>}></Route>
-            <Route path="/help" element={<Help/>}></Route>
-        </Routes>
+  return (
+    <>
+      <Container className="d-flex justify-content-center align-items-center">
+        <Row>
+          <Col>
+            <Link to={"/"}>Home</Link>
+          </Col>
+          <Col>
+            <Link to={"/about"}>About</Link>
+          </Col>
+          <Col>
+            <Link to={"/store"}>Store</Link>
+          </Col>
+          <Col>
+            <Link to={"/help"}>Help</Link>
+          </Col>
+        </Row>
+      </Container>
     </>
+  );
 }
 
-export default Navbar
+export default Navbar;

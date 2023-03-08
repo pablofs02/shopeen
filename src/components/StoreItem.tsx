8f431d531531b65476c972a711ea061cda88faee
guillermo-tellRM <guillermotellg@gmail.com>
2023-03-08 10:04:57 +0100

    New advances on StoreItem component

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Button, Card } from "react-bootstrap";
+import { useState } from "react";
 
 type StoreItemsProps = {
   id: number;
@@ -10,20 +11,36 @@ type StoreItemsProps = {
 };
 
 export function StoreItem(props: StoreItemsProps) {
+  const [showMore, setShowMore] = useState(false);
+
   return (
     <Card className="h-100 p-2">
-      <Card.Img
-        variant="top"
-        src={props.image}
-        height="200px"
-        style={{ objectFit: "contain" }}
-      />
-      <Card.Title className="fw-bold mt-3 d-flex justify-content-center text-center align-items-center">
-        {props.title}
-      </Card.Title>
-      <Card.Text className="d-flex justify-content-center align-items-center card-description mt-2">
-        {/* {props.description} */}
-      </Card.Text>
+      <Card.Body>
+        <Card.Img
+          variant="top"
+          src={props.image}
+          height="200px"
+          style={{ objectFit: "contain" }}
+        />
+        <Card.Title className="fw-bold mt-3 d-flex justify-content-center text-center align-items-center">
+          {props.title}
+        </Card.Title>
+        {showMore ? (
+          <Card.Text className="d-flex justify-content-center align-items-center card-description mt-2">
+            {props.description}
+          </Card.Text>
+        ) : (
+          <Card.Text className="d-flex justify-content-center align-items-center card-description mt-2">
+            {props.description.slice(0, 50)}...
+          </Card.Text>
+        )}
+        <Button
+          onClick={() => setShowMore(!showMore)}
+          variant="outline-primary"
+        >
+          {showMore ? "Show Less" : "Show More"}
+        </Button>
+      </Card.Body>
     </Card>
   );
 }

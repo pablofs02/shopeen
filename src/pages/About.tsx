aa4aa678686e92e20e0e23305442299f004335ea
javild01 <100673854+javild01@users.noreply.github.com>
2023-03-08 09:24:22 +0100

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,23 @@
-function About() {
-  return <h1>About</h1>
+function About(){
+    return(
+        <div>
+            <h1>About</h1>
+            <section>
+                <h2>Historia de la tienda</h2>
+                Esta es una tienda creada con el propósito de aprender React.
+            </section>
+            <section>
+                <h2>Equipo</h2>
+                Los creadores de esta aplicación somos:
+                <ul>
+                    <li>Pablo Fernández Serrano</li>
+                    <li>Guillermo TellA González</li>
+                    <li>Javier Lanceta Salas</li>
+                    <li>Javier Leiva Dueñas</li>
+                </ul>
+            </section>
+        </div>
+    );
 }
 
 export default About

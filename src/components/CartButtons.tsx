ab4a192cd3656d23f7f0e63833d24cb0951c1cbb
Javi <jlancetasalas@uma.es>
2023-03-24 21:13:24 +0100

    Added Tooltip when trying to add more items than stock available

--- src/components/CartButtons.tsx
+++ src/components/CartButtons.tsx
@@ -1,19 +1,26 @@
 import { useContext } from "react";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 import { CartItemContext } from "../context/CartItemContext";
 
-
 function CartButtons() {
-    
-    const { id, addItem, getQuantity, decreaseItemQuantity, stock } = useContext(CartItemContext);
+  const { id, addItem, getQuantity, decreaseItemQuantity, stock } = useContext(CartItemContext);
 
-    return (
+  const renderTooltip = <Tooltip>No more stock available</Tooltip>;
+
+  return (
     <div className="cart-buttons">
-        {getQuantity(id) === stock ? <p className="alert text-danger">Out of stock</p> : null}
-        <button disabled={getQuantity(id) === stock} className="btn btn-primary" onClick={() => {
-          if (getQuantity(id) === stock) return;
-          addItem(id)
-        }}>+</button>
-        <button className="btn btn-danger" onClick={() => {
+      <OverlayTrigger show={getQuantity(id) === stock} placement="bottom" overlay={renderTooltip}>
+        <button
+          disabled={getQuantity(id) === stock}
+          className="btn btn-primary"
+          onClick={() => {
+            if (getQuantity(id) === stock) return;
+            addItem(id);
+          }}>+</button>
+      </OverlayTrigger>
+      <button
+        className="btn btn-danger"
+        onClick={() => {
           if (getQuantity(id) === 0) return;
           decreaseItemQuantity(id);
         }}>-</button>

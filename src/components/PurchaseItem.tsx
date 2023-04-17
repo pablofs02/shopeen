b3aff5b6891bb710059a3a404053747afae161b7
guillermo-tellRM <guillermotellg@gmail.com>
2023-04-17 21:42:28 +0200

    Fixed PurchaseItem

--- src/components/PurchaseItem.tsx
+++ src/components/PurchaseItem.tsx
@@ -1,42 +1,38 @@
 import { Card } from "react-bootstrap";
 import { formatCurrency } from "../utilities/formatCurrency";
-import { useGlobalContext } from "../context/GlobalContext";
-import { useFilterContext } from "../context/FilterContext";
+import storeItems from "../data/products.json";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 type PurchaseItemProps = {
   id: number;
-  title: string;
-  price: number;
-  description: string;
-  category: string;
-  image: string;
-  stock: number;
   quantity: number;
 };
 
 export function PurchaseItem(props: PurchaseItemProps) {
-
+  
+    const [item, setItem] = useState(storeItems.find((item) => item.id === props.id)!);
+    
   return (
     <Card className="h-100 p-3 pt-4 pb-4 store-item">
       <Link to={"/store"}>
         <Card.Img
           variant="top"
-          src={props.image}
+          src={item.image}
           height="170px"
           style={{ objectFit: "contain" }}
         />
         <Card.Body className="d-flex flex-column h-100 p-0 ps-2 pe-2">
           <Card.Title className="mt-4 d-flex justify-content-between align-items-baseline">
-            <span>{props.title}</span>
+            <span>{item.title}</span>
             <span className="fw-bold price fs-5 ms-3 text-muted">
-              {formatCurrency(props.price)}
+              {formatCurrency(item.price)}
             </span>
           </Card.Title>
           <Card.Text className="desc-store d-flex justify-content-center align-items-center card-description mt-3 mb-auto">
-            {props.description.length > 120
-              ? props.description.slice(0, 120) + "..."
-              : props.description}
+            {item.description.length > 120
+              ? item.description.slice(0, 120) + "..."
+              : item.description}
           </Card.Text>
         </Card.Body>
       </Link>

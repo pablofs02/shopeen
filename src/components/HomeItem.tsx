aca824374126520e027ab238b1cea7a43c4f6f88
PabloFS <pablofs02@tutanota.com>
2023-04-18 17:17:46 +0200

    Home items fixed

--- src/components/HomeItem.tsx
+++ src/components/HomeItem.tsx
@@ -1,8 +1,7 @@
 import { Card } from "react-bootstrap";
 import { formatCurrency } from "../utilities/formatCurrency";
-import { useGlobalContext } from "../context/GlobalContext";
 import { useFilterContext } from "../context/FilterContext";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 type HomeItemsProps = {
   id: number;
@@ -14,38 +13,33 @@ type HomeItemsProps = {
   stock: number;
 };
 
+
 export function HomeItem(props: HomeItemsProps) {
-  const { searchItem } = useGlobalContext();
+  const navigate = useNavigate();
   const { setSearch } = useFilterContext();
 
   const handleSelection = (e: any) => {
     e.preventDefault();
+    navigate("/store");
     setSearch(props.title);
   };
 
   return (
     <Card onClick={handleSelection} className="h-100 p-3 pt-4 pb-4 store-item">
-      <Link to={"/store"}>
         <Card.Img
           variant="top"
           src={props.image}
           height="170px"
           style={{ objectFit: "contain" }}
         />
-        <Card.Body className="d-flex flex-column h-100 p-0 ps-2 pe-2">
-          <Card.Title className="mt-4 d-flex justify-content-between align-items-baseline">
+        <Card.Body className="d-flex flex-column p-0 ps-2 pe-2">
+          <Card.Title className="mt-4 d-flex justify-content-between align-items-baseline fw-bold">
             <span>{props.title}</span>
             <span className="fw-bold price fs-5 ms-3 text-muted">
               {formatCurrency(props.price)}
             </span>
           </Card.Title>
-          <Card.Text className="desc-store d-flex justify-content-center align-items-center card-description mt-3 mb-auto">
-            {props.description.length > 120
-              ? props.description.slice(0, 120) + "..."
-              : props.description}
-          </Card.Text>
         </Card.Body>
-      </Link>
     </Card>
   );
 }

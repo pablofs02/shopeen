adb462540ba10551eb9ba75d9550c7a1ffa28124
guillermo-tellRM <guillermotellg@gmail.com>
2023-05-09 18:52:30 +0200

    Added dependencies to test with vitest

---
+++ src/test/storeItem.test.tsx
@@ -0,0 +1,17 @@
+import { Mock, expect, test } from 'vitest';
+import { render, screen } from "@testing-library/react"
+import Filter from "../components/Filter";
+import { describe } from "vitest";
+import { FilterProvider } from '../context/FilterContext';
+
+describe("Filter", () => {
+    test("Filter", () => {
+        
+        render(
+            <FilterProvider>
+                <Filter />
+            </FilterProvider>
+        )
+        expect(screen.getByText("Filter options...")).toBeDefined();
+    })
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { test } from 'vitest';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/ProyIU/',
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./src/test/setUp.ts"],
+  },
 })

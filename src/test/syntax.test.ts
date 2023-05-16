433b1283ce193a12f6225f8e4e0daeb336b616cd
Javi Lanceta <jlancetasalas@uma.es>
2023-05-16 11:20:23 +0200

    Changed syntax test

--- src/test/syntax.test.ts
+++ src/test/syntax.test.ts
@@ -1,42 +1,49 @@
-import { spyOn } from '@vitest/spy';
-import { test, expect } from 'vitest';
-
+import { spyOn } from "@vitest/spy";
+import { test, expect, describe } from "vitest";
 
 const evenFunction = {
     even(a: number) {
         return a % 2 === 0;
-    }
-}
+    },
+};
 
-test('test numbers', () => {
-    expect(1 + 1).toBe(2);
-})
-
-test('test string', () => {
-    expect("hello").toBe("hello");
-})
-
-test('test even()', () => {
-    expect(evenFunction.even(2)).toBe(true);
-    expect(evenFunction.even(3)).toBe(false);
-    expect(evenFunction.even(4)).not.toBe(false);
-})
-
-test('test number of times a function is called', () => {
-    const evenSpy = spyOn(evenFunction, 'even');
+describe("Set of tests", () => {
     
-    for (let i = 0; i < 10; ++i) {
-        evenFunction.even(i);
-    }
+    test("test numbers", () => {
+        expect(1 + 1).toBe(2);
+    });
 
-    expect(evenSpy).toBeCalledTimes(10);
-})
+    test("test string", () => {
+        expect("hello").toBe("hello");
+    });
 
-test('test falsy and truthy values', () => {
-    expect(0).toBeFalsy();
-    expect("").toBeFalsy();
-    expect("hello").toBeTruthy();
-    expect(null).toBeFalsy();
-    expect(undefined).toBeFalsy();
-    expect([]).toBeTruthy();
-})
+    test("test even()", () => {
+        expect(evenFunction.even(2)).toBe(true);
+        expect(evenFunction.even(3)).toBe(false);
+        expect(evenFunction.even(4)).not.toBe(false);
+    });
+});
+
+
+describe("Set of tests", () => {
+    
+    test("test number of times a function is called", () => {
+        const evenSpy = spyOn(evenFunction, "even");
+
+        for (let i = 0; i < 10; ++i) {
+            evenFunction.even(i);
+        }
+
+        expect(evenSpy).toBeCalledTimes(10);
+    });
+
+    test("test falsy and truthy values", () => {
+        expect(0).toBeFalsy();
+        expect("").toBeFalsy();
+        expect("hello").toBeTruthy(); 
+        // expect("hello").toBeFalsy();
+        expect(null).toBeFalsy();
+        expect(undefined).toBeFalsy();
+    });
+
+});
